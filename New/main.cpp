4838777aea5787cb24a66a8615bee28c0c988594
Alberto <aclsobrinho@hotmail.com>
2018-10-15 11:39:10 -0300

    Menu

--- New/main.cpp
+++ New/main.cpp
@@ -1,23 +1,37 @@
 #include <iostream>
+#include <stdio.h>
 #include "tree.h"
-#include <unistd.h>
+//#include <unistd.h>
 using namespace std;
 
 int main(){
 
   tree arv;                              
-	
-  cout << "legend" << endl;
-  cout << "[X] = X is Red" << endl;
-  cout << "(X) = X is Black" << endl << endl; 
 
-  arv.insert(20);
-  arv.insert(10);
-  arv.insert(30);
-  arv.insert(5);
-  arv.remove(5);
-  arv.remove(10);
-
-   return 0;
+  int option = 0;
+  int key;
+  while(option != 5){
+    printf("\n1. Insert node.\n2. Remove node.\n3. Search node.\n4. Show tree.\n5. Quit.\n:>");
+    scanf("%i", &option);
+    
+    if (option==5){
+      //DO NOTHING
+    }else if (option==4){
+      arv.printTree();
+    }else if (option>=1 and option<=3){
+        printf("Key:>");
+        scanf("%i", &key);
+        if (option==1){
+          arv.insert(key);
+        }else if (option==2){
+          arv.remove(key);
+        }else{
+          arv.search(key);
+        }
+    }else{
+      printf("Option does not exist");
+    }
+  }
+  return 0;
 }  
 

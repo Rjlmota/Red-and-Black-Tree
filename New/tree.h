c077416b6007391a2af1e031c66acac493cdbe75
Alberto <aclsobrinho@hotmail.com>
2018-10-15 11:16:11 -0300

    Organização

--- New/tree.h
+++ New/tree.h
@@ -2,11 +2,6 @@
 #include "node.h"
 using namespace std;
 
-//FIXME LEFTROTATION
-//ADDME RIGHT ROTATION
-
-
-
 class tree {
 	private:  
 		node* root;
@@ -14,26 +9,28 @@ class tree {
 	public:
 
 		tree(); //Constructor
-
 		~tree(); //Destructor
 
-		void printTree(); // Function to print tree.
 		void eraseData(node *localRoot); // Function used by destructor.
+
+		void printTree(); // Function to print tree.
+
 		node* search(int key); // Function to search for node.
+		
 		node* getParent(int key); // Function used to get the node's parent.
 		node* getGrampa(int key); // Function used to get the node grampa.
 		node* getUncle(int key); // Function used to get the node uncle.
 		node* getSibling(int key); // Function used to get the node sibling.
-		node* getSucessor(node* currentN); // Function to get a sucessor of a node.
-		void insert(int key); // Function to insert a node.
-		void balance(node* currentN); // Function to check the balance.
-		void remove(int key); // Function for removal of node.
-		void switchColor(node* currentN); // Function to switch color.
+		node* getSucessor(int key); // Function to get a sucessor of a node.
+		
 		void leftRotation(node **p); // Function to do a left rotation on a tree.
 		void rightRotation(node **p); // Function to do a right rotation on a tree.
 		void del(node *p); // Function to delete a node.
 		void replace(node* currentN, node* sucessorN); // Function to replace a node with it's sucessor.
 
+		void insert(int key); // Function to insert a node.
+		void balance(node* currentN); // Function to check the balance.
+		void remove(int key); // Function for removal of node.
 };	
 
 
@@ -41,9 +38,7 @@ tree::tree(){
 	root = NULL;
 }
 
-tree::~tree(){
-	eraseData(root);
-}
+tree::~tree(){}
 
 node* tree::search(int key){
 	node* currentN = root;
@@ -57,111 +52,61 @@ node* tree::search(int key){
 	return currentN;
 }
 
-void tree::leftRotation(node **p0){
-	cout << "Left rotation on " << (*p0)->getKey() << ".\n";
-	node* parent = getParent((*p0)->getKey());
-	node *p1=NULL, *p2=NULL;
-	p1 = (*p0)->getRightChild();
-	p2 = p1->getLeftChild();
-	p1->setLeftChild(*p0);
-	(*p0)->setRightChild(p2);
-
-	if(parent != NULL){
-		if(parent->getLeftChild() == (*p0)){
-			parent->setLeftChild(p1);
-		}
-		else if(parent->getRightChild() == (*p0)){
-			parent->setRightChild(p1);
-		}
-	}else{
-		root = p1;
-	}
-}
-
-void tree::rightRotation(node **p0){
-	cout << "Right rotation on " << (*p0)->getKey() << ".\n";
-	node* parent = getParent((*p0)->getKey());
-	node *p1=NULL, *p2=NULL;
-	p1 = (*p0)->getLeftChild();
-	p2 = p1->getRightChild();
-	p1->setRightChild(*p0);
-	(*p0)->setLeftChild(p2);
-
-	if(parent != NULL){
-		if(parent->getLeftChild() == (*p0)){
-		parent->setLeftChild(p1);
-		}
-		else if(parent->getRightChild() == (*p0)){
-			parent->setRightChild(p1);
-		}
-	}else{
-		root = p1;
-	}
-}
-
-void tree::switchColor(node *currentN){
-	if(currentN->getColor() == 'r'){
-		currentN->setColor('b');
-	}else{
-		currentN->setColor('r');
-	}
-}
-
 void tree::printTree(){
 
-      stack<node*> pilhaGlobal;
-      pilhaGlobal.push(root);
-      int nVazios = 32;
-      bool linhaVazia = false;
+	cout << "\n";
 
-      cout << endl;
-      while(linhaVazia==false)
-         {
-         stack<node*> pilhaLocal;
-         linhaVazia = true;
+	stack<node*> pilhaGlobal;
+	pilhaGlobal.push(root);
+	int nVazios = 32;
+	bool linhaVazia = false;
+	
+	cout << endl;
+	while(linhaVazia==false){
+	stack<node*> pilhaLocal;
+	linhaVazia = true;
 
-         for(int j=0; j<nVazios; j++)
-            cout << ' ';
+		for(int j=0; j<nVazios; j++)
+			cout << ' ';
 
-         while(pilhaGlobal.empty()==false)
-            {
-            node* temp = pilhaGlobal.top();
-            pilhaGlobal.pop();
-            if(temp->isNotNull()){
-            	if (temp->getColor() == 'r'){
-            		cout << '['<< temp->getKey() << ']';	
-							}else if(temp->getColor() == 'b'){
-								cout << '('<< temp->getKey() << ')';
-							}else{
-								cout<< "((" << temp->getKey() << "))";
-							}
-              
-              pilhaLocal.push(temp->getLeftChild());
-              pilhaLocal.push(temp->getRightChild());
+		while(pilhaGlobal.empty()==false){
 
-               if(temp->getLeftChild()->isNotNull() || temp->getRightChild()->isNotNull())
-                  linhaVazia = false;
-               }
-            else
-               {
-               cout << "--";
-               node *nullNode = new node();
-               pilhaLocal.push(nullNode);
-               pilhaLocal.push(nullNode);
-               }
-            for(int j=0; j<nVazios*2-2; j++)
-               cout << ' ';
-            }  
-         cout << endl;
-         nVazios = nVazios/2;
-         while(pilhaLocal.empty()==false)
-            {
-            pilhaGlobal.push( pilhaLocal.top() );
-            pilhaLocal.pop();
-            }
-         }  
+			node* temp = pilhaGlobal.top();
+			pilhaGlobal.pop();
+			if(temp->isNotNull()){
+				if (temp->getColor() == 'r'){
+					cout << '['<< temp->getKey() << ']';	
+				}else if(temp->getColor() == 'b'){
+					cout << '('<< temp->getKey() << ')';						
+				}else{
+					cout<< "((" << temp->getKey() << "))";	
+				}
 
-      cout << endl;
+				pilhaLocal.push(temp->getLeftChild());
+				pilhaLocal.push(temp->getRightChild());
+
+				if(temp->getLeftChild()->isNotNull() || temp->getRightChild()->isNotNull())
+					linhaVazia = false;
+			}
+			else{
+				cout << "(  )";
+				node *nullNode = new node();
+				pilhaLocal.push(nullNode);
+				pilhaLocal.push(nullNode);
+			}
+			for(int j=0; j<nVazios*2-2; j++)
+				cout << ' ';
+		}
+
+		cout << endl;
+		nVazios = nVazios/2;
+		while(pilhaLocal.empty()==false){
+			pilhaGlobal.push( pilhaLocal.top() );
+			pilhaLocal.pop();
+		}
+	}  
+	cout << endl;
+	cout << "[ ] = Red | ( ) = Black | (( )) = DoubleBlack\n...\n";
 }  
 
 node* tree::getParent(int key){
@@ -210,8 +155,7 @@ node* tree::getSibling(int key){
 		return parentN->getRightChild();
 }
 
-node* tree::getSucessor(node* p){
-	int key = p->getKey();
+node* tree::getSucessor(int key){
 	node* currentN = search(key);
 	node* sucessorN = currentN->getLeftChild();
 	if (sucessorN->isNotNull()){
@@ -221,108 +165,46 @@ node* tree::getSucessor(node* p){
 	return sucessorN;
 }
 
-void tree::insert(int key){
-   	
-	node *newN = new node(key);
+void tree::leftRotation(node **p0){
+	cout << "Left rotation on " << (*p0)->getKey() << ".\n";
+	node* parent = getParent((*p0)->getKey());
+	node *p1=NULL, *p2=NULL;
+	p1 = (*p0)->getRightChild();
+	p2 = p1->getLeftChild();
+	p1->setLeftChild(*p0);
+	(*p0)->setRightChild(p2);
 
-	if (root == NULL){
-		
-		root = newN;
-	
-	}else{	
-	
-		node* parentN = getParent(key);
-		if (key > parentN->getKey())
-			parentN->setRightChild(newN);
-		else{
-			parentN->setLeftChild(newN);
+	if(parent != NULL){
+		if(parent->getLeftChild() == (*p0)){
+			parent->setLeftChild(p1);
 		}
-	
+		else if(parent->getRightChild() == (*p0)){
+			parent->setRightChild(p1);
+		}
+	}else{
+		root = p1;
 	}
-	cout << "Insert key: " << key << endl;
-	printTree();
-	cout << "Balancing tree." << endl;
-	balance(newN);
-	printTree();
 }
 
-void tree::balance(node* currentN){
-	if (currentN->isNotNull()){
-		node* parentN = getParent(currentN->getKey());
-		node* uncleN = getUncle(currentN->getKey());
-		node* grampaN = getGrampa(currentN->getKey());
+void tree::rightRotation(node **p0){
+	cout << "Right rotation on " << (*p0)->getKey() << ".\n";
+	node* parent = getParent((*p0)->getKey());
+	node *p1=NULL, *p2=NULL;
+	p1 = (*p0)->getLeftChild();
+	p2 = p1->getRightChild();
+	p1->setRightChild(*p0);
+	(*p0)->setLeftChild(p2);
 
-		/* CASE 1: NODE IS ROOT: */
-		if (parentN == NULL){
-			cout << "Case 1." << endl;
-			/* NODE CHANGES TO BLACK */
-			currentN->setColor('b');
-		}else{
-			/* CASE 2: NODE'S PARENT IS BLACK */
-			if (parentN->getColor()=='b'){
-				cout << "Case 2." << endl;
-				/* DONT TO ANYTHING */
-			}
-			/* CASE 3: PARENT AND UNCLE ARE RED. */
-			else if (parentN->getColor()=='r'){
-				if ((uncleN->isNotNull()) and (uncleN->getColor()=='r')){
-					/* 
-						CHANGE PARENT AND UNCLE TO BLACK.
-				        CHANGE GRANDFATHER TO RED. 
-					*/
-					cout << "Case 3." << endl;
-					parentN->setColor('b');
-					uncleN->setColor('b');
-					grampaN->setColor('r');
-					balance(grampaN);
-					
-				} else if ((uncleN->isNull()) or (uncleN->getColor()=='b')){
-					/* CASE 4: PARENT IS RED AND UNCLE IS BLACK. */	
-					/* 
-						STEP 1: IF THE INSERTED NODE IS INSIDE GRANDPARENT'S SUBTREE.
-				       	DO: ROTATE INSERTED AND PARENT.
-		
-					*/
-					cout << "Case 4." << endl;
-					cout << "Step 1." << endl;
-
-					node *temp = NULL;
-					int flag = 0;
-
-					if(grampaN->getRightChild() == parentN){
-						if(currentN == parentN->getLeftChild()){
-							rightRotation(&parentN);
-							flag = 1;
-						}
-					}
-					else if(grampaN->getLeftChild() == parentN){
-						if(currentN == parentN->getRightChild()){
-							leftRotation(&parentN); 
-							flag = 1;
-						}
-					}
-
-					if(flag == 0){
-						temp = parentN;
-						parentN = currentN;
-						currentN = temp;
-					}
-
-				    //STEP 2: ROTATE PARENT AND GRAMPARENT, AND CHANGE COLORS.
-				    cout << "Step 2." << endl;
-					switchColor(grampaN);
-					if(grampaN->getLeftChild() == currentN){
-						rightRotation(&grampaN);
-					}else if(grampaN->getRightChild() == currentN){
-						leftRotation(&grampaN);
-					}
-
-					switchColor(currentN);
-
-				}
-			}		
+	if(parent != NULL){
+		if(parent->getLeftChild() == (*p0)){
+		parent->setLeftChild(p1);
 		}
-	}		
+		else if(parent->getRightChild() == (*p0)){
+			parent->setRightChild(p1);
+		}
+	}else{
+		root = p1;
+	}
 }
 
 void tree::del(node* currentN){
@@ -367,86 +249,219 @@ void tree::replace(node* currentN, node* sucessorN){
 	currentN->~node();
 }	
 
+void tree::insert(int key){
+   	
+	node *newN = new node(key);
+
+	if (root == NULL){
+		
+		root = newN;
+	
+	}else{	
+	
+		node* parentN = getParent(key);
+		if (key > parentN->getKey())
+			parentN->setRightChild(newN);
+		else{
+			parentN->setLeftChild(newN);
+		}
+	
+	}
+	cout << "Inserting key: " << key << ".\n";
+	printTree();
+	cout << "Balancing tree." << endl;
+	balance(newN);
+	printTree();
+}
+
+void tree::balance(node* currentN){
+	if (currentN->isNotNull()){
+		node* parentN = getParent(currentN->getKey());
+		node* uncleN = getUncle(currentN->getKey());
+		node* grampaN = getGrampa(currentN->getKey());
+
+		/* CASE 1: NODE IS ROOT: */
+		if (parentN == NULL){
+			cout << "Case 1." << endl;
+			/* NODE CHANGES TO BLACK */
+			currentN->setColor('b');
+		}else{
+			/* CASE 2: PARENT IS BLACK */
+			if (parentN->getColor()=='b'){
+				cout << "Case 2." << endl;
+				/* DONT TO ANYTHING */
+			}
+			/* CASE 3: PARENT IS RED */
+			else{
+				cout << "Case 3.";
+				/* CASE 3.1: UNCLE IS RED */
+				if (uncleN->getColor()=='r'){
+					/* 
+						CHANGE PARENT AND UNCLE TO BLACK.
+				        CHANGE GRANDFATHER TO RED. 
+					*/
+					cout << "1.\n";
+					parentN->setColor('b');
+					uncleN->setColor('b');
+					grampaN->setColor('r');
+					printTree();
+					balance(grampaN);
+				}
+				/* CASE 3.2: UNCLE IS BLACK */
+				else{
+					cout << "2.\n";
+					/* 
+						STEP 1: 
+						IF THE INSERTED NODE IS INSIDE GRANDPARENT'S SUBTREE.
+				       	ROTATE INSERTED AND PARENT.
+					*/
+					cout << "Step 1.\n";
+
+					node *temp = NULL;
+					int flag = 0;
+
+					if(grampaN->getRightChild() == parentN){
+						if(currentN == parentN->getLeftChild()){
+							rightRotation(&parentN);
+							flag = 1;
+						}
+					}
+					else if(grampaN->getLeftChild() == parentN){
+						if(currentN == parentN->getRightChild()){
+							leftRotation(&parentN); 
+							flag = 1;
+						}
+					}
+
+					if(flag == 0){
+						temp = parentN;
+						parentN = currentN;
+						currentN = temp;
+					}
+
+				    /* STEP 2: ROTATE PARENT AND GRAMPARENT, AND CHANGE COLORS. */
+				    cout << "Step 2." << endl;
+					grampaN->switchColor();
+					if(grampaN->getLeftChild() == currentN){
+						rightRotation(&grampaN);
+					}else if(grampaN->getRightChild() == currentN){
+						leftRotation(&grampaN);
+					}
+					currentN->switchColor();
+				}
+			}		
+		}
+	}		
+}
+
 void tree::remove(int key){
-	//Adapt to RBT
+	
+	cout << "Removing key: " << key << ".\n";
+
 	node* currentN = search(key);
-	node* sucessorN = getSucessor(currentN);
+	node* sucessorN = getSucessor(currentN->getKey());
 	node* sublingN = getSibling(currentN->getKey());
 	
-	//Standard BST Removal.
+	/* STEP 1: STANDARD BST REMOVAL */
 	replace(currentN, sucessorN);
-	printTree();
 
-	//Check if either removed ou replacer are red
-	if(currentN->getColor() == 'r' or sucessorN->getColor() == 'r')
+	/* STEP 2: CHECK REMOVED AND SUCESSOR */
+	/* CASE 1: IF REMOVED OR SUCESSOR IS RED */
+	if(currentN->getColor() == 'r' or sucessorN->getColor() == 'r'){
+		cout << "Case 1.\n";
 		sucessorN->setColor('b');
+		printTree();
+	}
 
-	//v is deleted and u is replacer
+	/* CASE 2: IF REMOVED AND SUCESSOR ARE BLACK */
 	else if(currentN->getColor() =='b' and sucessorN->getColor()=='b'){
+		/* COLOR SUCESSOR AS DOUBLEBLACK */
 		cout << "DoubleBlack condition.\n";
 		sucessorN->setColor('B'); //Double black
+		printTree();
 	}
-	while (sucessorN->getColor()=='B'){
+	/* WHILE THE DOUBLEBLACK CONDITION EXISTS */
+	while (sucessorN->getColor()=='B'){	
 		
+		/* CASE 2.1: IF SUCESSOR IS ROOT */
 		if (sucessorN == root){
+			/* REMOVE DOUBLEBLACK */
+			cout << "Case 2.1.\n";
 			sucessorN->setColor('b');
 		}
 		else{
 			
+			cout << "Case 2.2.";
+
 			node* siblingN = getSibling(sucessorN->getKey());
 			node* parentN = getParent(sucessorN->getKey());
 			node* leftchildN = siblingN->getLeftChild();
 			node* rightchildN = siblingN->getRightChild();
-
+			/* CASE 2.2.a||b: IF SIBLING IS BLACK */
 			if (siblingN->getColor()=='b'){
 				
+				/* CASE 2.2.a: IF AT LEAST ONE CHILDREN IS RED */
 				if (leftchildN->getColor()=='r' or rightchildN->getColor()=='r'){
-					
+					cout << "a.\n.";
+					/* CASE 2.2.a.i: SUCESSOR IS A LEFT CHILD AND REDCHILD IS A LEFT CHILD */
 					if (siblingN==parentN->getLeftChild() and leftchildN->getColor()=='r'){
+						/* RIGHT ROTATION ON PARENT AND RECOLOR CHILD */
 						rightRotation(&parentN);
-						switchColor(leftchildN);
+						leftchildN->switchColor();
 					}
+					/* CASE 2.2.a.ii: SUCESSOR IS A LEFT CHILD AND REDCHILD IS A RIGHT CHILD */
 					else if (siblingN==parentN->getLeftChild() and rightchildN->getColor()=='r'){
+						/* DOUBLE RIGHT ROTATION ON PARENT AND RECOLOR CHILD*/
 						leftRotation(&siblingN);
 						rightRotation(&parentN);
-						switchColor(rightchildN);
+						rightchildN->switchColor();
 					}
-					
+					/* CASE 2.2.a.iii: SUCESSOR IS A RIGHTCHILD AND REDCHILD IS A RIGHT CHILD */
 					if (siblingN==parentN->getRightChild() and rightchildN->getColor()=='r'){
+						/* LEFT ROTATION ON PARENT AND RECOLOR CHILD */
 						leftRotation(&parentN);
-						switchColor(rightchildN);
+						rightchildN->switchColor();
 					}
+					/* CASE 2.2.a.iv: SUCESSOR IS A RIGHTCHILD AND REDCHILD IS A LEFT CHILD */
 					else if (siblingN==parentN->getRightChild() and leftchildN->getColor()=='r'){
+						/* DOUBLE LEFT ROTATION ON PARENT AND RECOLOR CHILD */
 						rightRotation(&siblingN);
 						leftRotation(&parentN);
-						switchColor(leftchildN);
+						leftchildN->switchColor();
 					}
+					/* REMOVE DOUBLEBLACK */
 					sucessorN->setColor('b');
 				}
+				/*CASE 2.2.b: BOTH CHILDREN ARE BLACK*/
 				else if (leftchildN->getColor()=='b' and rightchildN->getColor()=='b'){
-					switchColor(siblingN);
+					cout << "b.\n";
+					/* RECOLOR SIBLING */
+					siblingN->switchColor();
+					/* IF PARENT IS RED, RECOLOR AND REMOVE DOUBLEBLACK */
 					if (parentN->getColor()=='r'){
-						switchColor(parentN);
+						parentN->switchColor();
 						sucessorN->setColor('b');
-					}else{
+					}
+					/* IF PARENT IS BLACK, RECUR TO PARENT */
+					else{
 						sucessorN = parentN;
 						sucessorN->setColor('B');
 					}
 				}
-			}else{
-				
+			}
+			/* CASE 2.2.c: IF SIBLING IS RED */
+			else{
+				cout << ".c.\n";
+				/* ROTATE SIBLING AND RECOLOR SIBLING AND PARENT */
 				if (siblingN==parentN->getLeftChild())
 					rightRotation(&parentN);
 				else
 					leftRotation(&parentN);	
-					
-				switchColor(siblingN);
-				switchColor(parentN);
-							
+				
+				siblingN->switchColor();
+				parentN->switchColor();							
 			}
 		}
-	printTree();
+		printTree();
 	}
 }
-
-
